b40533c25ed69f7ae3af7639773619fbe337812c
Yeehahn <2000359@apps.nsd.org>
2025-03-25 08:53:18 -0700

    Selecting Datasets

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pandas as pd
 '''
 Create amazing plots in this file. You will read the data from `data_organized` 
 (unless your raw data required no reduction, in which case you can read your data from `raw_data`). 
 You will do plot-related work such as joins, column filtering, pivots, 
 small calculations and other simple organizational work. 
 '''
+
